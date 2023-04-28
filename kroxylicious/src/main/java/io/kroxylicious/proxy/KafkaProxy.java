0261f86953f615a03022579c624c5ea9ec1fa8ac
Keith Wall <kwall@apache.org>
2023-04-28 12:05:33 +0100

    extract some vars for clarity

--- kroxylicious/src/main/java/io/kroxylicious/proxy/KafkaProxy.java
+++ kroxylicious/src/main/java/io/kroxylicious/proxy/KafkaProxy.java
@@ -9,6 +9,7 @@ import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 import java.util.Queue;
 import java.util.Set;
 import java.util.concurrent.ConcurrentLinkedQueue;
