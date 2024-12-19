c7eedf8cd9ef97dcc0e214f5c7a369f342e53f08
alanrobinson-dwp <alan.robinson3@engineering.digital.dwp.gov.uk>
2024-12-19 09:12:17 +0000

    Update kroxylicious-runtime/src/test/java/io/kroxylicious/proxy/filter/DenyCipherSuiteFilterTest.java
    Co-authored-by: Keith Wall <kwall@redhat.com>
    Signed-off-by: alanrobinson-dwp <alan.robinson3@engineering.digital.dwp.gov.uk>

--- kroxylicious-runtime/src/test/java/io/kroxylicious/proxy/filter/DenyCipherSuiteFilterTest.java
+++ kroxylicious-runtime/src/test/java/io/kroxylicious/proxy/filter/DenyCipherSuiteFilterTest.java
@@ -15,7 +15,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
-public class DenyCipherSuiteFilterTest {
+class DenyCipherSuiteFilterTest {
 
     @Test
     public void emptyCiphersDefaultsToDefaultCiphers() {
