b386b5d5a026bb13e40277d6da2dc1668f4408c0
Tom Bentley <tbentley@redhat.com>
2026-02-12 12:43:23 +1300

    Add hasAtLeastOneEntityField to krpc model; omit generating empty loops
    Signed-off-by: Tom Bentley <tbentley@redhat.com>

--- kroxylicious-krpc-plugin/src/main/java/io/kroxylicious/krpccodegen/model/FieldSpecModel.java
+++ kroxylicious-krpc-plugin/src/main/java/io/kroxylicious/krpccodegen/model/FieldSpecModel.java
@@ -5,10 +5,19 @@
  */
 package io.kroxylicious.krpccodegen.model;
 
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import io.kroxylicious.krpccodegen.schema.EntityType;
 import io.kroxylicious.krpccodegen.schema.FieldSpec;
 
+import freemarker.ext.beans.GenericObjectModel;
 import freemarker.template.AdapterTemplateModel;
+import freemarker.template.ObjectWrapperAndUnwrapper;
+import freemarker.template.SimpleSequence;
 import freemarker.template.TemplateHashModel;
+import freemarker.template.TemplateMethodModelEx;
 import freemarker.template.TemplateModel;
 import freemarker.template.TemplateModelException;
 
@@ -30,6 +39,7 @@ class FieldSpecModel implements TemplateHashModel, AdapterTemplateModel {
             case "typeString" -> wrapper.wrap(spec.typeString());
             case "about" -> wrapper.wrap(spec.about());
             case "entityType" -> wrapper.wrap(spec.entityType());
+            case "hasAtLeastOneEntityField" -> wrapper.wrap((TemplateMethodModelEx) this::handleHasAtLeastOneEntityField);
             case "flexibleVersions" -> wrapper.wrap(spec.flexibleVersions());
             case "flexibleVersionsString" -> wrapper.wrap(spec.flexibleVersionsString());
             case "defaultString" -> wrapper.wrap(spec.defaultString());
@@ -51,6 +61,47 @@ class FieldSpecModel implements TemplateHashModel, AdapterTemplateModel {
     @Override
     public boolean isEmpty() {
         return false;
+    }
+
+    private boolean handleHasAtLeastOneEntityField(List args) throws TemplateModelException {
+        var seq = argsToSimpleSequence(args);
+        var set = getTypeHashSet(seq);
+        return spec.hasAtLeastOneEntityField(set);
+    }
+
+    private SimpleSequence argsToSimpleSequence(List args) throws TemplateModelException {
+        var seq = new SimpleSequence(wrapper);
+        for (Object objOrSeq : args) {
+            if (objOrSeq instanceof SimpleSequence ss) {
+                for (int i = 0; i < ss.size(); i++) {
+                    var obj = ss.get(i);
+                    seq.add(obj);
+                }
+            }
+            else if (objOrSeq instanceof GenericObjectModel gom) {
+                seq.add(gom);
+            }
+            else {
+                throw new TemplateModelException("Unsupported argument type " + objOrSeq.getClass().getName() + " found in arguments.");
+            }
+        }
+        return seq;
+    }
+
+    private static Set<EntityType> getTypeHashSet(SimpleSequence seq) throws TemplateModelException {
+        var ow = (ObjectWrapperAndUnwrapper) seq.getObjectWrapper();
+        var set = new HashSet<EntityType>(seq.size());
+        for (int i = 0; i < seq.size(); i++) {
+            try {
+                TemplateModel obj = seq.get(i);
+                var unwrapped = ow.unwrap(obj);
+                set.add(unwrapped instanceof EntityType et ? et : EntityType.valueOf(String.valueOf(unwrapped)));
+            }
+            catch (TemplateModelException e) {
+                throw new TemplateModelException("Failed to unwrap template model object at index " + i, e);
+            }
+        }
+        return set;
     }
 
     @Override
